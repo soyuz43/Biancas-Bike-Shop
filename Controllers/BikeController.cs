67d18abde6379e019ffb66390c623f40c00c8712
soyuz43 <kebekad673@proton.me>
2025-05-28 09:23:20 -0500

    Modified Bike controller

--- Controllers/BikeController.cs
+++ Controllers/BikeController.cs
@@ -22,17 +22,67 @@ public class BikeController : ControllerBase
     [Authorize]
     public IActionResult Get()
     {
-        return Ok(_dbContext
+        var bikes = _dbContext
             .Bikes
+            .Include(b => b.Owner)
             .Select(b => new BikeDTO
             {
                 Id = b.Id,
                 Brand = b.Brand,
                 Color = b.Color,
                 BikeTypeId = b.BikeTypeId,
-                OwnerId = b.OwnerId
+                OwnerId = b.OwnerId,
+                Owner = new OwnerDTO
+                {
+                    Id = b.Owner.Id,
+                    Name = b.Owner.Name
+                }
             })
-            .ToList());
+            .ToList();
+
+        return Ok(bikes);
     }
 
+    [HttpGet("{id}")]
+    [Authorize]
+    public IActionResult GetById(int id)
+    {
+        var bike = _dbContext
+            .Bikes
+            .Include(b => b.Owner)
+            .Include(b => b.BikeType)
+            .Include(b => b.WorkOrders)
+            .SingleOrDefault(b => b.Id == id);
+
+        if (bike == null) return NotFound();
+
+        var dto = new BikeDTO
+        {
+            Id = bike.Id,
+            Brand = bike.Brand,
+            Color = bike.Color,
+            BikeTypeId = bike.BikeTypeId,
+            OwnerId = bike.OwnerId,
+            BikeType = new BikeTypeDTO
+            {
+                Id = bike.BikeType.Id,
+                Name = bike.BikeType.Name
+            },
+            Owner = new OwnerDTO
+            {
+                Id = bike.Owner.Id,
+                Name = bike.Owner.Name,
+                Address = bike.Owner.Address
+            },
+            WorkOrders = bike.WorkOrders.Select(wo => new WorkOrderDTO
+            {
+                Id = wo.Id,
+                Description = wo.Description,
+                DateInitiated = wo.DateInitiated,
+                DateCompleted = wo.DateCompleted
+            }).ToList()
+        };
+
+        return Ok(dto);
+    }
 }
